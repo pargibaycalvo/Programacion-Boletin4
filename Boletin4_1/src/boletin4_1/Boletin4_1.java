6ef9452e0a99c28ade235cb296daf5be47578574
Pedro Jose Argibay Calvo <>
2016-10-18 18:54:34 +0200

    boletin4_1

---
+++ Boletin4_1/src/boletin4_1/Boletin4_1.java
@@ -0,0 +1,17 @@
+
+package boletin4_1;
+
+public class Boletin4_1 {
+
+    public static void main(String[] args) {
+    //Coche
+    Coche coche1 = new Coche ();
+    
+    coche1.acelerar(200);
+    coche1.frenar(50);
+       
+    
+    System.out.println("Velocidad = " + coche1.getVelocidade());
+    }
+    
+}
