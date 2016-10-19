726d2d1f84caf548147dc7dbef25942589a6f941
Pedro Jose Argibay Calvo <Ped90@Ped90>
2016-10-19 08:51:02 +0200

    boletin4_2

---
+++ Boletin4_2/src/boletin4_2/Boletin4_2.java
@@ -0,0 +1,14 @@
+
+package boletin4_2;
+
+public class Boletin4_2 {
+
+   public static void main(String[] args) {
+       Satelite novoSat = new Satelite(84,50,75);
+       novoSat.verPosicion();
+    }
+    
+}
+    
+    
+
